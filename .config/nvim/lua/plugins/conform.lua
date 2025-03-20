39fcff2e1a04080f3237bc6bc3e1eb43bd6219ef
DMelisena <demonlordx777@gmail.com>
2025-03-20 23:00:19 +0800

    Change conform to ios-dev-starter-nvim

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -8,15 +8,18 @@ return {
       formatters_by_ft = {
         swift = { "swiftformat" },
       },
-      format_on_save = function(bufnr)
-        local ignore_filetypes = { "oil" }
-        if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
-          return
-        end
-
+      format_on_save = function()
         return { timeout_ms = 500, lsp_fallback = true }
       end,
       log_level = vim.log.levels.ERROR,
     })
+
+    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+      })
+    end, { desc = "Format file or range (in visual mode)" })
   end,
 }
