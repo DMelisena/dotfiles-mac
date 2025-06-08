ce78f02d90267649dcb30507ece5ced365d4b50a
DMelisena <demonlordx777@gmail.com>
2025-06-08 16:11:35 +0800

    Change trouble config

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -1,42 +1,37 @@
 return {
   "folke/trouble.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  event = { "BufReadPre", "BufNewFile" },
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
   keys = {
-    { "<leader>tt", "<cmd>Trouble quickfix toggle<cr>", { desc = "Open a quickfix" } },
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
   },
-
-  opts = {},
-  config = function()
-    require("trouble").setup({
-      auto_open = false,
-      auto_close = false,
-      auto_preview = true,
-      auto_jump = false,
-      mode = "quickfix",
-      severity = vim.diagnostic.severity.ERROR,
-      cycle_results = false,
-    })
-
-    vim.api.nvim_create_autocmd("User", {
-      pattern = { "XcodebuildBuildFinished", "XcodebuildTestsFinished" },
-      callback = function(event)
-        if event.data.cancelled then
-          return
-        end
-
-        if event.data.success then
-          require("trouble").close()
-        elseif not event.data.failedCount or event.data.failedCount > 0 then
-          if next(vim.fn.getqflist()) then
-            require("trouble").open("quickfix")
-          else
-            require("trouble").close()
-          end
-
-          require("trouble").refresh()
-        end
-      end,
-    })
-  end,
 }
