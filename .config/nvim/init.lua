f21e9c0e0f6466040fbba8a715c649a043c4606e
DMelisena <demonlordx777@gmail.com>
2025-07-23 11:40:17 +0800

    feat: vim shortcut mapping

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,3 +5,7 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*",
   command = ":%s/\\s\\+$//e",
 })
+vim.g.mapleader = " "
+local map = function(mode, keys, func, desc)
+  vim.keymap.set(mode, keys, func, { desc = desc })
+end
