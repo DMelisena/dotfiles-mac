d746b51e4c21a70d625bc8723ac0639133c6ddea
DMelisena <demonlordx777@gmail.com>
2025-07-23 11:44:47 +0800

    feat: window resizing feature

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -9,3 +9,21 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+-- resize with arrows
+vim.keymap.set("n", "<A-Down>", "<cmd>resize -4<cr>", { desc = "Smaller horizontal split" })
+vim.keymap.set("n", "<A-Up>", "<cmd>resize +4<cr>", { desc = "Bigger horizontal split" })
+vim.keymap.set("n", "<A-Left>", "<cmd>vertical resize -4<cr>", { desc = "Smaller vertical split" })
+vim.keymap.set("n", "<A-Right>", "<cmd>vertical resize +4<cr>", { desc = "Bigger vertical split" })
+
+vim.keymap.del("n", "<A-j>") -- Removes normal mode mapping for Ctrl + j
+vim.keymap.del("n", "<A-k>")
+vim.keymap.del("n", "<A-l>")
+vim.keymap.del("n", "<A-h>")
+-- resize with vim movemenet
+vim.keymap.set("n", "<c-j>", "<cmd>resize -4<cr>", { desc = "Smaller horizontal split" })
+vim.keymap.set("n", "<C-k>", "<cmd>resize +4<cr>", { desc = "Bigger horizontal split" })
+vim.keymap.set("n", "<A-l>", "<cmd>vertical resize -4<cr>", { desc = "Smaller vertical split" })
+vim.keymap.set("n", "<A-h>", "<cmd>vertical resize +4<cr>", { desc = "Bigger vertical split" })
+vim.keymap.set("n", "<A-h>", "<cmd>vertical resize +4<cr>", { desc = "Bigger vertical split" })
+vim.keymap.del("n", "<leader>e")
