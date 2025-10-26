a67145b288f916bd3b0e8bcf3d9b28126462803c
DMelisena <demonlordx777@gmail.com>
2025-10-26 22:29:32 +0800

    feat: install molten for ipynb/py functions in vim

---
+++ .config/nvim/lua/plugins/molten.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "benlubas/molten-nvim",
+    version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+    dependencies = { "3rd/image.nvim" },
+    build = ":UpdateRemotePlugins",
+    init = function()
+      -- these are examples, not defaults. Please see the readme
+      vim.g.molten_image_provider = "image.nvim"
+      vim.g.molten_output_win_max_height = 20
+    end,
+    keys = {
+      { "<leader>mi", ":MoltenInit<CR>", desc = "Initialize Molten" },
+      { "<leader>me", ":MoltenEvaluateLine<CR>", mode = "n", desc = "Molten Evaluate Line" },
+      { "<leader>me", ":<C-u>MoltenEvaluateVisual<CR>gv", mode = "v", desc = "Molten Evaluate Visual" },
+      { "<leader>mr", ":MoltenReevaluateCell<CR>", desc = "Molten Re-evaluate Cell" },
+      { "<leader>md", ":MoltenDelete<CR>", desc = "Molten Delete Cell" },
+      { "<leader>mo", ":MoltenHideOutput<CR>", desc = "Molten Hide Output" },
+      { "<leader>ms", ":MoltenEnterOutput<CR>", desc = "Molten Show/Enter Output" },
+    },
+  },
+  {
+    -- see the image.nvim readme for more information about configuring this plugin
+    "3rd/image.nvim",
+    opts = {
+      backend = "kitty", -- whatever backend you would like to use
+      max_width = 100,
+      max_height = 12,
+      max_height_window_percentage = math.huge,
+      max_width_window_percentage = math.huge,
+      window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
+      window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+    },
+  },
+}
