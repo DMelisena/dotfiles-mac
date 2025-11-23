eeeda5a62d0e83906faa4de9e0734c35225a6947
DMelisena <demonlordx777@gmail.com>
2025-11-23 20:24:47 +0800

    fix: overlapping icon and red wifi when unactivated

--- .config/sketchybar/plugins/wifi.sh
+++ .config/sketchybar/plugins/wifi.sh
@@ -8,5 +8,7 @@ WIFI_SSID=$(ipconfig getsummary en0 | awk -F' SSID : ' '/ SSID : / {print $2}')
 if [ "$WIFI_STATUS" = "On" ] && [ -n "$WIFI_SSID" ]; then
   sketchybar --set "$NAME" icon="󰖩" label="$WIFI_SSID" icon.color=0xffffffff label.color=0xffffffff label.drawing=off
 else
-  sketchybar --set "$NAME" icon="󰖪" label="Off" icon.color=0xffbf616a label.color=0xffbf616a label.drawing=off
+  sketchybar --set "$NAME" icon="󰖩" label="Off" icon.color=0xa9a9a9a9 label.color=0xffbf616a label.drawing=off icon.font.size=18
+  #󰖪
 fi
+
